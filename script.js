07c98539739782bcc8f20c0b4b4bd991ced0d4c5
Teddy <tedloukas@outlook.com>
2022-04-24 15:25:37 +0900

    add word input

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-const scoreboard = (function () {
-  console.log("Hello");
+const documentSelector = (() => {
+  let words = [];
+  input = document.querySelector(".word-input");
+  wordList = document.querySelector(".new-words");
+
+  input.addEventListener("keypress", function (e) {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      console.log(input.value);
+      words.push(input.value);
+      wordList.textContent = words;
+      input.value = "";
+    }
+
+    // DELETE LAST WORD
+  });
+
+  return function (name) {
+    greeting = console.log(`hello ${name}`);
+  };
 })();
 
-const documentSelector = (() => {
-  input = document.querySelector(".word-input");
-
-  return function () {};
+const scoreboard = (function () {
+  console.log("Hello");
 })();
